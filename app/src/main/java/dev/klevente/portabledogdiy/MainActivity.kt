4ec32e1a29f8f5f5b2ed892891ef107c3ce0cf6b
Levente Krizsan <lewalevi@gmail.com>
2021-03-29 21:26:25 +0200

    feat(arch): add basic list

--- app/src/main/java/dev/klevente/portabledogdiy/MainActivity.kt
+++ app/src/main/java/dev/klevente/portabledogdiy/MainActivity.kt
@@ -2,10 +2,16 @@ package dev.klevente.portabledogdiy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.zsmb.rainbowcake.navigation.SimpleNavActivity
+import dev.klevente.portabledogdiy.ui.list.ListFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : SimpleNavActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            navigator.add(ListFragment())
+        }
     }
 }
