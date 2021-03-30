febde9193494f54fcdad95d9ea08056a13516c2c
Levente Krizsan <lewalevi@gmail.com>
2021-03-30 14:26:35 +0200

    feat(arch): add about screen components

---
+++ app/src/main/java/dev/klevente/portabledogdiy/ui/about/AboutFragment.kt
@@ -0,0 +1,6 @@
+package dev.klevente.portabledogdiy.ui.about
+
+import androidx.fragment.app.Fragment
+import dev.klevente.portabledogdiy.R
+
+class AboutFragment : Fragment(R.layout.fragment_about)
